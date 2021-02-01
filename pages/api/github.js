a08c9508b311dc66b3adef1f23f1fc7b5622cb2c
Alexs <alexs@safebreach.com>
2021-02-01 15:51:13 +0200

    test signature

--- pages/api/github.js
+++ pages/api/github.js
@@ -5,25 +5,26 @@ import MemoryStore from '../../lib/store';
 const secret = 'UYTlk9Hdybmq5LUMzC';
 const store = new MemoryStore();
 
-function calculatePayloadSignature(payload) {
-  const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex');
+function calculatePayloadSignature(body) {
+  const signature = crypto.createHmac('sha256', secret).update(body).digest('hex');
   return `sha256=${signature}`;
 }
 
 export default async (req, res) => {
-  const signature = calculatePayloadSignature(req.body.payload);
-  if (req.headers['x-hub-signature-256'] !== `sha256=${signature}`) {
-    return res.status(500).json({ message: 'Bad secret' });
-  } 
-  const parsedPayload = JSON.parse(req.body.payload);
-  if (req.headers['x-github-event'] === 'pull_request' && parsedPayload.action === 'closed') {
+  const { body } = req;
+  const signature = calculatePayloadSignature(body);
+  if (req.headers['x-hub-signature-256'] !== signature) {
+    return res.status(500).json({ message: 'Bad signature' });
+  }
+
+  if (req.headers['x-github-event'] === 'pull_request' && body.action === 'closed') {
     const pullRequest = { 
       source: 'github',
-      action: parsedPayload.action,
-      externalId: parsedPayload.pull_request.id,      
-      merged_at: parsedPayload.pull_request.merged_at,
-      merged_by: parsedPayload.pull_request.merged_by.login,
-      html_url: parsedPayload.pull_request.html_url
+      action: body.action,
+      externalId: body.pull_request.id,      
+      merged_at: body.pull_request.merged_at,
+      merged_by: body.pull_request.merged_by.login,
+      html_url: body.pull_request.html_url
     }
     await store.closePullRequest(pullRequest);
 
