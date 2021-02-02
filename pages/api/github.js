cad6ce0b1751fc95bfdae183e76a93764a1f23dd
Alexs <alexs@safebreach.com>
2021-02-02 10:58:20 +0200

    evidence API

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import crypto from 'crypto';
+import request from 'request';
+import { once } from 'events';
 import store from '../../lib/store';
 
 function calculatePayloadSignature(body) {
@@ -20,7 +22,7 @@ export default async (req, res) => {
   if (req.headers['x-github-event'] === 'pull_request' && body.action === 'closed') {
     const pullRequest = { 
       source: 'github',
-      title: body.title,
+      title: body.pull_request.title,
       action: body.action,
       externalId: body.pull_request.id,      
       merged_at: body.pull_request.merged_at,
@@ -28,8 +30,27 @@ export default async (req, res) => {
       html_url: body.pull_request.html_url,
       _raw: body,
     }
-    await store.closePullRequest(pullRequest);
-
+    await Promise.all([
+      store.closePullRequest(pullRequest),
+      saveEvidence(pullRequest.externalId, pullRequest.html_url),
+    ])
   }
   res.status(200).json({ name: 'Github', status: 'Ok' });
+}
+
+async function saveEvidence(id, html_url) {
+  const uploadStream = await store.getUploadEvidenceStream(id);
+  const r = request
+    .get(`http://localhost:8080/?type=screenshot&fullPage=true&url=${html_url}`)
+    .on('response', function(response) {
+      console.log(response.statusCode)
+      console.log(response.headers['content-type'])
+    })
+    .on('data', function(chunk) {
+      uploadStream.write(chunk)
+    })
+    .on('end', function() {
+      uploadStream.end();
+    })
+    return once(r, 'end');
 }
